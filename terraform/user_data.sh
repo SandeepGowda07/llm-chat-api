a27797c6f3739d87afc40341a1a20a057c442476
SandeepGowda07 <sandeepagowda25@gmail.com>
2026-04-15 20:29:55 +0200

    fix: modified k3s installation with public ip

--- terraform/user_data.sh
+++ terraform/user_data.sh
@@ -11,8 +11,12 @@ mkswap /swapfile
 swapon /swapfile
 echo '/swapfile none swap sw 0 0' >> /etc/fstab   # persist across reboots
 
-# Install k3s (lightweight Kubernetes)
-curl -sfL https://get.k3s.io | sh -
+# Get the public IP of this EC2 instance from AWS metadata
+PUBLIC_IP=$(curl -s http://169.254.169.254/latest/meta-data/public-ipv4)
+
+# Install k3s with the public IP added to the TLS certificate (SAN)
+# Without this, remote kubectl connections fail with a certificate error
+curl -sfL https://get.k3s.io | sh -s - --tls-san $PUBLIC_IP
 
 # Wait for k3s to be ready
 sleep 30
